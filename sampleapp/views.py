aa344e2828c38293ae693a41dd7e03fb92f65697
1F10210016 <85672265+1F10210016@users.noreply.github.com>
2021-12-23 01:17:10 +0900

    henkou

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.http import Http404
+from django.utils import timezone
+import random
+from blog.models import Article
 
 # Create your views here.
+def index(request):
+    if request.method=='POST':
+        article=Article(title=request.POST['title'],body=request.POST['text'])
+        article.save()
+        return redirect(detail, article.id)
+    context = {
+        "articles": Article.objects.all()
+    }
+    return render(request, 'blog/index.html', context)
+
+def detail(request, article_id):
+    try:
+        article=Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist")
+    context={
+        'article':article,
+    }
+    return render(request, "blog/detail.html", context)
