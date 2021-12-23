f48e3c30c869b2986c5799501c001228fff2ff4e
YusukeKADO <s1f101903861@iniad.org>
2021-12-23 11:20:17 +0900

    commit on Dec 23

--- sampleapp/urls.py
+++ sampleapp/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='home'),
+	path('', views.apphome, name='apphome'),
+    path('index', views.index, name='index'),
+    path('redirect', views.redirect_test, name='redirect_test'),
+    path('<int:article_id>/', views.detail, name='detail'),
+    path('<int:article_id>/delete', views.delete, name='delete'),
+    path('<int:article_id>/update', views.delete, name='update'),
 ]
--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.http import Http404
+
+from blog.models import Article
 
 # Create your views here.
+
+def apphome(request):
+    return render(request, 'teamapp/apphome.html', {})
+
+def index(request):
+    context = {
+        "articles": Article.objects.all()
+    }
+    return render(request, 'teamapp/index.html', context)
+    
+    """
+    if request.method == 'GET':
+        return render(request, 'teamapp/index.html', {})
+    elif request.method == 'POST':
+        title = request.POST['title']
+        text = request.POST['task']
+        return HttpResponse('Title: {}, Task: {}'.format(title, text))
+    """
+
+def redirect_test(request):
+    return redirect(apphome)
+
+def detail(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist.")
+    context = {
+        "article": article
+    }
+    return render(request, 'teamapp/tbd.html', context)
+
+def update(request, article_id):
+    context = {
+        "article_id": article_id
+    }
+    return render(request, 'teamapp/tbd.html', context)
+
+def delete(request, article_id):
+    return redirect(index)
