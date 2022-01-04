46b9dfba27703bf35d5e431185b8a89e5456ccf3
YusukeKADO <s1f101903861@iniad.org>
2022-01-04 14:14:11 +0900

    Merge branch 'main' of https://github.com/YusukeKADO/team-project into main

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import Http404
-<<<<<<< HEAD
 
 from .forms import ArticleCreateForm
 
@@ -30,29 +29,14 @@ def index(request):
         media = Media(title=request.POST['title'], image=request.POST['image'])
         media.save()
         return redirect(detail, article.id, media.id)
-=======
-from django.utils import timezone
-import random
-from sampleapp.models import Article
-
-# Create your views here.
-def index(request):
-    if request.method=='POST':
-        article=Article(title=request.POST['title'],body=request.POST['text'])
-        article.save()
-        return redirect(detail, article.id)
->>>>>>> 6d9121448ef78e7a760c324615df55681f96e8c3
     context = {
         "articles": Article.objects.all(),
         "media": Media.objects.all(),
     }
     return render(request, 'sampleapp/index.html', context)
-<<<<<<< HEAD
 
 def redirect_test(request):
     return redirect(index)
-=======
->>>>>>> 6d9121448ef78e7a760c324615df55681f96e8c3
 
 def detail(request, article_id):
     try:
@@ -62,7 +46,6 @@ def detail(request, article_id):
     context={
         'article':article,
     }
-<<<<<<< HEAD
     return render(request, 'sampleapp/detail.html', context)
 
 def update(request, article_id):
@@ -98,6 +81,3 @@ def media_create(request):
     else:
         form = MediaCreateForm()
     return render(request, 'sampleapp/media_create.html', {'form':form})
-=======
-    return render(request, "blog/detail.html", context)
->>>>>>> 6d9121448ef78e7a760c324615df55681f96e8c3
